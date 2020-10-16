69346334242f6bff415363252202afa15388e2b0
Tom Roar Furunes <tom.roar@halloverden.no>
2020-10-16 15:05:46 +0200

    temp save

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,13 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
  * Class TestController
  *
  * @package App\Controller
- *
- * @Route(path="/test", methods={"GET"}, name="testlol")
  */
 class TestController extends AbstractController {
 
-  public function __invoke(): Response {
-    return new Response("hei");
+  /**
+   * @Route("/test", name="testxd")
+   */
+  public function testing() {
+    return new Response('ok');
   }
 
 }
